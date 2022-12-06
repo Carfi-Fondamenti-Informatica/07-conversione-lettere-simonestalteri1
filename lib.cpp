8336f0b3734f4f53d91e69e5f0411fd45a1ed319
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-12-06 20:18:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,18 +1,12 @@
-
 #include "lib.h"
-bool conversione (char &a) {
-bool t=true;
-  if(a>=65 and a<=90 or a>=97 and a<=122){
-    t=true;
-    if(a>=65 and a<=90){
-      a=a+32;
-      return &a;
+
+bool funzione(char carattereinserito){
+    char caratterestampa;
+    bool risultato;
+    if (carattereinserito>=65&&carattereinserito<=90 or carattereinserito>=97&&carattereinserito<=122){
+        risultato=true;
     } else {
-      a=a-32;
-      return &a;
+        risultato=false;
     }
-  }else{
-    t=false;
-  }
-  return t;
+    return risultato;
 }
