2b0db688e768bc3910c6fbcf9dfe8e5fcc69a823
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-12-06 20:13:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
 
+using namespace std;
 int main(){
+  char a=a;
+  cin>>a;
+  if (conversione (a)==false){
+    cout << "errore";
+  }else{
+    cout <<a;
+  }
   return 0;
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Term
